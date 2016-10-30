47a31a545edfcacc2a11d6b4025eaf5d015706d8
MIHIR PATEL <mihirptl89@gmail.com>
2016-10-30 17:37:36 -0500

    settin up backend data

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 'use strict';
 var express = require('express'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    civic = require('./lib/services/civicService');
 
+// Initiate App, body-parser and path to public
 var app = express(),
     port = process.env.PORT_NUMBER || 8080;
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
-//Endpoints
+// Endpoints
+app.post('/api/representatives', civic.getReps);
 
-//DB
+// Initiate Port
+app.listen(port, function(){
+   console.log('Hello ' + port);
+});
